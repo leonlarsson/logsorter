a5ab1ab52f218bf64c17bb82e395ac449860617e
Leon San José Larsson <leonlarsson8@gmail.com>
2022-03-15 19:13:40 +0100

    update getRegex and parseTextBox
    Adds better checks and such...

--- assets/js/getRegex.js
+++ assets/js/getRegex.js
@@ -1,6 +1,9 @@
 /**
  * Determines which Regex is to use.
- * @returns {RegExp} The regex.
+ * @typedef {Object} ReturnObject
+ * @property {RegExp} regex The regex selected. Null if checks failed.
+ * @property {string} regexId The number of the regex selected.
+ * @returns {ReturnObject} { regex, regexNum }
  */
 export default () => {
   const customEntry1 = $("#customEntry1").val();
@@ -9,123 +12,127 @@ export default () => {
   const customEntry4 = $("#customEntry4").val();
   const customEntry5 = $("#customEntry5").val();
   let customEntry;
-  let regex;
+  let regex = null;
+  const regexId = $("#regexList").val();
 
   if ($("#checkBoxMultiple").is(":checked")) {
-    if (customEntry1 == "" || customEntry2 == "" || customEntry3 == "" || customEntry4 == "" || customEntry5 == "") {
+
+    // If there are empty ID fields, retun (as null)
+    if (customEntry1 === "" || customEntry2 === "" || customEntry3 === "" || customEntry4 === "" || customEntry5 === "") {
       alert("Please fill in all the fields or decrease the number of fields.");
-      return;
+      return { regex, regexId };
     }
 
-    if ($('#multipleIDs').children('input').length == 2) {
+    if ($("#multipleIDs").children("input").length === 2) {
       customEntry = `(?:${customEntry1}|${customEntry2})`;
     }
-    if ($('#multipleIDs').children('input').length == 3) {
+    if ($("#multipleIDs").children("input").length === 3) {
       customEntry = `(?:${customEntry1}|${customEntry2}|${customEntry3})`;
     }
-    if ($('#multipleIDs').children('input').length == 4) {
+    if ($("#multipleIDs").children("input").length === 4) {
       customEntry = `(?:${customEntry1}|${customEntry2}|${customEntry3}|${customEntry4})`;
     }
-    if ($('#multipleIDs').children('input').length == 5) {
+    if ($("#multipleIDs").children("input").length === 5) {
       customEntry = `(?:${customEntry1}|${customEntry2}|${customEntry3}|${customEntry4}|${customEntry5})`;
     }
 
   } else {
-    customEntry = $("#customEntry").val(); // Get first ID numbers to match
+    // Get first ID numbers to match
+    customEntry = $("#customEntry").val();
   }
 
 
-  if ($("#regexList").val() == 1) { // Zeppelin/Aperture/Auttaja - Join/Leave
+  // Zeppelin/Aperture/Auttaja - Join/Leave
+  if ($("#regexList").val() === "1") {
     regex = new RegExp(`#.+[^\\d](${customEntry}\\d+)\\).+(?:left|joined)`, "g");
-    console.log("--------------------------------------\n[DEBUG] RegEx 1 was selected.");
   }
 
-  if ($("#regexList").val() == 2) { // Zeppelin/Aperture/Auttaja - Join
+  // Zeppelin/Aperture/Auttaja - Join
+  if ($("#regexList").val() === "2") {
     regex = new RegExp(`#.+[^\\d](${customEntry}\\d+)\\).+joined`, "g");
-    console.log("--------------------------------------\n[DEBUG] RegEx 2 was selected.");
   }
 
-  if ($("#regexList").val() == 3) { // Zeppelin/Aperture/Auttaja - Leave
+  // Zeppelin/Aperture/Auttaja - Leave
+  if ($("#regexList").val() === "3") {
     regex = new RegExp(`#.+[^\\d](${customEntry}\\d+)\\).+left`, "g")
-    console.log("--------------------------------------\n[DEBUG] RegEx 3 was selected.");
   }
 
-  if ($("#regexList").val() == 4) { // Utilibot - Join/Leave
+  // Utilibot - Join/Leave
+  if ($("#regexList").val() === "4") {
     regex = new RegExp(`ID:\\s(${customEntry}\\d+)[^#]+#[^#]+(?:left|joined)`, "g");
-    console.log("--------------------------------------\n[DEBUG] RegEx 4 was selected.");
   }
 
-  if ($("#regexList").val() == 5) { // Utilibot - Join
+  // Utilibot - Join
+  if ($("#regexList").val() === "5") {
     regex = new RegExp(`ID:\\s(${customEntry}\\d+)[^#]+#[^#]+joined`, "g");
-    console.log("--------------------------------------\n[DEBUG] RegEx 5 was selected.");
   }
 
-  if ($("#regexList").val() == 6) { // Utilibot - Leave
+  // Utilibot - Leave
+  if ($("#regexList").val() === "6") {
     regex = new RegExp(`ID:\\s(${customEntry}\\d+)[^#]+#[^#]+left`, "g");
-    console.log("--------------------------------------\n[DEBUG] RegEx 6 was selected.");
   }
 
-  if ($("#regexList").val() == 7) { // Vortex - Join/Leave
-    regex = new RegExp(`(?::inbox|:outbox).+#\\d+\\s\\(ID:(${customEntry}\\d+)`, "g")
-    console.log("--------------------------------------\n[DEBUG] RegEx 7 was selected.");
+  // Vortex - Join/Leave
+  if ($("#regexList").val() === "7") {
+    regex = new RegExp(`(?::inbox|:outbox).+#\\d+\\s\\(ID:(${customEntry}\\d+)`, "g");
   }
 
-  if ($("#regexList").val() == 8) { // Vortex - Join
+  // Vortex - Join
+  if ($("#regexList").val() === "8") {
     regex = new RegExp(`:inbox.+#\\d+\\s\\(ID:(${customEntry}\\d+)`, "g");
-    console.log("--------------------------------------\n[DEBUG] RegEx 8 was selected.");
   }
 
-  if ($("#regexList").val() == 9) { // Vortex - Leave
+  // Vortex - Leave
+  if ($("#regexList").val() === "9") {
     regex = new RegExp(`:outbox.+#\\d+\\s\\(ID:(${customEntry}\\d+)`, "g");
-    console.log("--------------------------------------\n[DEBUG] RegEx 9 was selected.");
   }
 
-  if ($("#regexList").val() == 10) { // Vortex - Kicks
+  // Vortex - Kicks
+  if ($("#regexList").val() === "10") {
     regex = new RegExp(`#\\d+\\skicked.+#\\d+\\s\\(ID:(${customEntry}\\d+)`, "g");
-    console.log("--------------------------------------\n[DEBUG] RegEx 10 was selected.");
   }
 
-  if ($("#regexList").val() == 95) { // Nexus - Join/Leave
+  // Nexus - Join/Leave
+  if ($("#regexList").val() === "95") {
     regex = new RegExp(`#+\\d+\\s\\[(${customEntry}\\d+)]\\s(?:joined|left)`, "g");
-    console.log("--------------------------------------\n[DEBUG] RegEx 95 was selected.");
   }
 
-  if ($("#regexList").val() == 96) { // Nexus - Join
+  // Nexus - Join
+  if ($("#regexList").val() === "96") {
     regex = new RegExp(`#+\\d+\\s\\[(${customEntry}\\d+)]\\sjoined`, "g");
-    console.log("--------------------------------------\n[DEBUG] RegEx 96 was selected.");
   }
 
-  if ($("#regexList").val() == 97) { // Nexus - Leave
+  // Nexus - Leave
+  if ($("#regexList").val() === "97") {
     regex = new RegExp(`#+\\d+\\s\\[(${customEntry}\\d+)]\\sleft`, "g");
-    console.log("--------------------------------------\n[DEBUG] RegEx 97 was selected.");
   }
 
-  if ($("#regexList").val() == 98) { // Mee6/GiselleBot/Dyno/Carl-bot
+  // Mee6/GiselleBot/Dyno/Carl-bot
+  if ($("#regexList").val() === "98") {
     regex = new RegExp(`.*ID:\\s(${customEntry}\\d+)`, "g");
-    console.log("--------------------------------------\n[DEBUG] RegEx 98 was selected.");
   }
 
-  if ($("#regexList").val() == 99) { // Custom - Group 1
+  // Custom - Group 1
+  if ($("#regexList").val() === "99") {
     if (customEntry == "") {
-      alert("Please provide a RegEx when using Custom RegEx. Capturing group 1 will be matched.");
-      return;
+      alert("Please provide a RegEx when using Custom RegEx. Capturing group 1 will be matched.\n\nExamples:\n(\\d+)\nID: (\\d+)\n#\\d{4} \\((\\d{17,19})\\)");
     } else {
       regex = new RegExp(customEntry, "g");
-      console.log("--------------------------------------\n[DEBUG] Custom RegEx (99) was selected.");
     }
   }
 
   // TODO: Monitor the length of Discord IDs. Currently the majority are 17-18.
   // Update - Oct 20, 2021: Updated regexs below to support up to 19.
-  if ($("#regexList").val() == 100) { // All IDs - First occurrence. 
+
+  // All IDs - First occurrence
+  if ($("#regexList").val() === "100") {
     regex = new RegExp(`^.*?\\b(${customEntry}\\d{${17 - customEntry.length},${19 - customEntry.length}})\\b`, "gm");
-    console.log("--------------------------------------\n[DEBUG] RegEx 100 was selected.");
   }
 
-  if ($("#regexList").val() == 101) { // All IDs
+  // All IDs
+  if ($("#regexList").val() === "101") {
     regex = new RegExp(`\\b(${customEntry}\\d{${17 - customEntry.length},${19 - customEntry.length}})\\b`, "g");
-    console.log("--------------------------------------\n[DEBUG] RegEx 101 was selected.");
   }
 
-  return regex;
+  return { regex, regexId };
 }
