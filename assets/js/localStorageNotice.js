c1282b29184e51df5b241b6f38be328e841dca5b
Leon San José Larsson <leonlarsson8@gmail.com>
2022-04-15 00:52:20 +0200

    remove some jQuery (loading and LS notice)

--- assets/js/localStorageNotice.js
+++ assets/js/localStorageNotice.js
@@ -1,10 +1,14 @@
+const notice = document.getElementById("localStorageConsentNotice");
+
 /** Hides the localStorage notice and sets the key */
 export function hideNotice() {
     localStorage.setItem("acknowledgedStorageNotice", "yup");
-    $("#localStorageConsentNotice").fadeOut(200);
+    notice.classList.add("hide");
+    notice.classList.remove("show");
+    setTimeout(() => notice.hidden = true, 200);
 };
 
-/** Shows the localStorage notice and sets the key */
+/** Shows the localStorage notice */
 export function showNotice() {
-    $("#localStorageConsentNotice").fadeIn(400);
+    notice.hidden = false;
 };
