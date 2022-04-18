36950755cf220144df54c4f7fdb80897651c169a
Leon San José Larsson <leonlarsson8@gmail.com>
2022-04-18 20:46:16 +0200

    remove jQuery from handleMultipleIDFields.js
    This is very scuffed.

--- assets/js/handleMultipleIDFields.js
+++ assets/js/handleMultipleIDFields.js
@@ -1,19 +1,25 @@
+const multipleIDs = document.getElementById("multipleIDs");
+
 /** Removes one multiple IDs field */
 export function addIDField() {
 
     // Increase the number when func is run. Used for the customEntryNUMBER id
-    let number = $('#multipleIDs').children('input').length + 1;
+    let number = document.querySelectorAll(".multiplesCustomEntry").length + 1;
 
     // If there are 5 ID Fields, don't add more
-    if ($('#multipleIDs').children('input').length == 5) {
+    if (document.querySelectorAll(".multiplesCustomEntry").length === 5) {
         number = 5;
         return;
     }
 
     if (window.isLightMode) {
-        $("#multipleIDs").append(`<label class="optionText lightMode">&</label><input class="inputBox multiplesCustomEntry lightMode" type="number" id="customEntry${number}" style="width: 60px; height: 20px; border-radius: 3px; margin-left: 5px; margin-right: 5px; border-color: #40444b; border: 0px; font-family: inherit;">`);
+        const template = document.createElement("template");
+        template.innerHTML = `<label class="optionText lightMode">&</label><input class="inputBox multiplesCustomEntry lightMode" type="number" id="customEntry${number}" style="width: 60px; height: 20px; border-radius: 3px; margin-left: 5px; margin-right: 5px; border-color: #40444b; border: 0px; font-family: inherit;">`;
+        multipleIDs.appendChild(template.content);
     } else {
-        $("#multipleIDs").append(`<label class="optionText">&</label><input class="inputBox multiplesCustomEntry" type="number" id="customEntry${number}" style="width: 60px; height: 20px; border-radius: 3px; margin-left: 5px; margin-right: 5px; border-color: #40444b; border: 0px; font-family: inherit;">`);
+        const template = document.createElement("template");
+        template.innerHTML = `<label class="optionText">&</label><input class="inputBox multiplesCustomEntry" type="number" id="customEntry${number}" style="width: 60px; height: 20px; border-radius: 3px; margin-left: 5px; margin-right: 5px; border-color: #40444b; border: 0px; font-family: inherit;">`;
+        multipleIDs.appendChild(template.content);
     }
 }
 
@@ -21,13 +27,12 @@ export function addIDField() {
 export function removeIDField() {
 
     // If there are 2 ID Fields, don't remove more
-    if ($('#multipleIDs').children('input').length == 2) {
+    if (document.querySelectorAll(".multiplesCustomEntry").length === 2) {
         number = 2;
         return;
     }
-    let idFields = document.getElementById('multipleIDs');
 
     // Remove 2. One ID Field and one & separator
-    idFields.removeChild(idFields.lastElementChild);
-    idFields.removeChild(idFields.lastElementChild);
+    multipleIDs.removeChild(multipleIDs.lastElementChild);
+    multipleIDs.removeChild(multipleIDs.lastElementChild);
 }
